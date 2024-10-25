b04ecc00a1e4020584d173005c5d456cedd5d93d
rbpatt2019 <rb.patterson.cross@gmail.com>
2024-10-25 12:25:55 +0100

    feat(repo): initialise repo

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "A nixvim configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixvim.url = "github:nix-community/nixvim";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixvim,
+      flake-parts,
+      pre-commit-hooks,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit pkgs;
+            module = import ./config; # import the module directly
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
+        {
+          checks = {
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+
+            pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+              src = ./.;
+              hooks = {
+                statix.enable = true;
+                nixfmt-rfc-style.enable = true;
+              };
+            };
+          };
+
+          formatter = pkgs.nixfmt-rfc-style;
+
+          packages.default = nvim;
+
+          devShells = {
+            default = with pkgs; mkShell { inherit (self'.checks.pre-commit-check) shellHook; };
+          };
+        };
+    };
+}
