74af6e6348d8aafb01991d216fb68dc92cf17973
rbpatt2019 <rb.patterson.cross@gmail.com>
2026-07-30 13:15:22 +0100

    refactor(flake.nix): update to modern flake parts structure

--- flake.nix
+++ flake.nix
@@ -10,67 +10,61 @@
   };
 
   outputs =
-    inputs@{
-      nixpkgs,
+    {
       flake-parts,
       nixvim,
       ...
-    }:
-    flake-parts.lib.mkFlake { inherit inputs; } (
-      {
-        ...
-      }:
-      {
-        imports = [
-          inputs.treefmt-nix.flakeModule
-          inputs.git-hooks-nix.flakeModule
-        ];
-        systems = nixpkgs.lib.systems.flakeExposed;
-        perSystem =
-          {
-            config,
-            pkgs,
-            system,
-            ...
-          }:
-          let
-            nixvimLib = nixvim.lib.${system};
-            nixvim' = nixvim.legacyPackages.${system};
-            nixvimModule = {
-              inherit pkgs;
-              module = import ./config;
-            };
-            nvim = nixvim'.makeNixvimWithModule nixvimModule;
-          in
-          {
-            packages.default = nvim;
-            checks.default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
-            treefmt = {
-              programs = {
-                nixfmt.enable = true;
-                deadnix.enable = true;
-                statix.enable = true;
-                rstfmt.enable = true;
-                yamlfmt.enable = true;
-              };
-            };
-            pre-commit.settings.hooks = {
-              check-added-large-files.enable = true;
-              check-executables-have-shebangs.enable = true;
-              check-shebang-scripts-are-executable.enable = true;
-              check-merge-conflicts.enable = true;
-              detect-private-keys.enable = true;
-              end-of-file-fixer.enable = true;
-              mixed-line-endings.enable = true;
-              trim-trailing-whitespace.enable = true;
-              treefmt = {
-                enable = true;
-                verbose = true;
-              };
-              flake-checker.enable = true;
-            };
-            devShells.default = config.pre-commit.devShell;
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.treefmt-nix.flakeModule
+        inputs.git-hooks-nix.flakeModule
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          system,
+          ...
+        }:
+        let
+          configuration = nixvim.lib.evalNixvim {
+            inherit system;
+            modules = [ ./config ];
           };
-      }
-    );
+        in
+        {
+          packages.default = configuration.config.build.package;
+          checks.default = configuration.config.build.test;
+          treefmt = {
+            programs = {
+              nixfmt.enable = true;
+              deadnix.enable = true;
+              statix.enable = true;
+              rstfmt.enable = true;
+              yamlfmt.enable = true;
+            };
+          };
+          pre-commit.settings.hooks = {
+            check-added-large-files.enable = true;
+            check-executables-have-shebangs.enable = true;
+            check-shebang-scripts-are-executable.enable = true;
+            check-merge-conflicts.enable = true;
+            detect-private-keys.enable = true;
+            end-of-file-fixer.enable = true;
+            mixed-line-endings.enable = true;
+            trim-trailing-whitespace.enable = true;
+            treefmt = {
+              enable = true;
+              verbose = true;
+            };
+            flake-checker.enable = true;
+          };
+          devShells.default = config.pre-commit.devShell;
+        };
+    };
 }
